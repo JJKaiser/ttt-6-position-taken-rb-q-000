fbbcdc357fd20c4cc46aa35bf0c96eeb869c3787
JJKaiser <justinjkaiser@gmail.com>
2016-04-24 13:52:15 +0000

    nitrous-backup at Sun Apr 24 13:52:15 UTC 2016

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,10 +1,15 @@
 # code your #position_taken? method here!
-
-def position_taken?(board, position)
-if board[position] == "" || board[position] == " " || board[position] == nil
-  return false
-elsif
-  board[position] == "X" || board[position] == "O"
-  return true
-end
+def valid_move?(board, position)
+  if board[position.to_s.between?(1, 9)]
+  def position_taken?(board, position)
+    if board[position.to_s - 1] == "" || board[position.to_s - 1] == " " || board[position.to_s - 1] == nil
+      return false
+    elsif board[position.to_s - 1] == "X" || board[position.to_s - 1] == "O"
+      return true
+    end
+  if position_taken?
+    return false
+  else
+    return true
+  end
 end
